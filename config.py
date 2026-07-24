ca9e345ade442868e981b62596d1976e8712e56e
Kanaad Limaye <kanaad.limaye@gmail.com>
2026-07-24 17:28:41 +0530

    Modified config.py file

--- config.py
+++ config.py
@@ -7,9 +7,9 @@ MOONDREAM_API_KEY = os.getenv("MOONDREAM_API_KEY")
 TEXT_EMBED_MODEL = "all-MiniLM-L6-v2"
 FASHION_CLIP_MODEL = "patrickjohncyh/fashion-clip"
 
-IMAGES_DIR = r"D:\Kanaad\Glance-Fashion\data\images"
-CHROMA_DIR = r"D:\Kanaad\Glance-Fashion\data\chroma_db"
-COLLECTION_NAME = "fashion_captions"
+IMAGES_DIR = r"\images_directory"
+CHROMA_DIR = r"\chromadb"
+COLLECTION_NAME = "image_captions"
 
 TOP_N_CANDIDATES = 20   # candidates pulled before rerank
 CAPTION_WEIGHT = 0.4
